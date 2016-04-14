83f8624ce4d73d732b2485bf1a0a4fe791d1807b
jaredrooprai <roopraijared@gmail.com>
2016-04-14 14:38:07 -0600

    new database dump .sql

--- hospitaldb.sql
+++ hospitaldb.sql
@@ -1,140 +1,344 @@
-CREATE Database IF NOT EXISTS hospitaldb;
-USE hospitaldb;
+-- MySQL dump 10.13  Distrib 5.7.11, for osx10.11 (x86_64)
+--
+-- Host: localhost    Database: hospitaldb
+-- ------------------------------------------------------
+-- Server version	5.7.11
 
-DROP TABLE IF EXISTS `hospital`;
-CREATE TABLE `hospital` (
-  `Address` varchar(255) NOT NULL,
-  `Name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`Address`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `appointment`
+--
+
+DROP TABLE IF EXISTS `appointment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `appointment` (
+  `room_number` int(11) unsigned NOT NULL,
+  `staff_staffID` int(11) unsigned NOT NULL,
+  `patient_SSN` int(11) unsigned NOT NULL,
+  `time` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointment`
+--
+
+LOCK TABLES `appointment` WRITE;
+/*!40000 ALTER TABLE `appointment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `appointment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `emergency_contact`
+--
+
+DROP TABLE IF EXISTS `emergency_contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `emergency_contact` (
+  `name` varchar(20) NOT NULL,
+  `phone_number` int(11) unsigned NULL,
+  `relationship` varchar(15) DEFAULT NULL,
+  `patient_ssn` int(9) unsigned NOT NULL,
+  PRIMARY KEY (`name`,`patient_ssn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `emergency_contact`
+--
+
+LOCK TABLES `emergency_contact` WRITE;
+/*!40000 ALTER TABLE `emergency_contact` DISABLE KEYS */;
+/*!40000 ALTER TABLE `emergency_contact` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `equipment`
+--
 
 DROP TABLE IF EXISTS `equipment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `equipment` (
-  `Name` varchar(255) NOT NULL,
-  `Manufacturer` varchar(255) NOT NULL,
-  `Type` varchar(255) DEFAULT NULL,
-  `Hospital_Address` varchar(255) NOT NULL,
-  Foreign KEY (`Hospital_Address`) references hospital(`Address`),
-  PRIMARY KEY (`Name`,`Manufacturer`,`Hospital_Address`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `name` varchar(20) NOT NULL,
+  `manufacturer` varchar(20) NOT NULL,
+  `type` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`name`,`manufacturer`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipment`
+--
+
+LOCK TABLES `equipment` WRITE;
+/*!40000 ALTER TABLE `equipment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hospital`
+--
+
+DROP TABLE IF EXISTS `hospital`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hospital` (
+  `address` varchar(40) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hospital`
+--
+
+LOCK TABLES `hospital` WRITE;
+/*!40000 ALTER TABLE `hospital` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hospital` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `machine`
+--
+
+DROP TABLE IF EXISTS `machine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `machine` (
+  `equipment_name` varchar(40) NOT NULL,
+  `equipment_manufacturer` varchar(40) NOT NULL,
+  `equipment_type` varchar(20) NOT NULL,
+  `room_number` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`equipment_name`,`equipment_manufacturer`,`equipment_type`,`room_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `machine`
+--
+
+LOCK TABLES `machine` WRITE;
+/*!40000 ALTER TABLE `machine` DISABLE KEYS */;
+/*!40000 ALTER TABLE `machine` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `medical_staff`
+--
+
+DROP TABLE IF EXISTS `medical_staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medical_staff` (
+  `specialization` varchar(20) NOT NULL,
+  `staff_staffid` int(10) unsigned NOT NULL,
+  `staff_access` varchar(20) DEFAULT NULL,
+  `staff_salary` int(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medical_staff`
+--
+
+LOCK TABLES `medical_staff` WRITE;
+/*!40000 ALTER TABLE `medical_staff` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medical_staff` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `other_staff`
+--
+
+DROP TABLE IF EXISTS `other_staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `other_staff` (
+  `title` varchar(10) NOT NULL,
+  `staff_staffID` int(11) NOT NULL,
+  `staff_access` varchar(20) DEFAULT NULL,
+  `staff_salary` int(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `other_staff`
+--
+
+LOCK TABLES `other_staff` WRITE;
+/*!40000 ALTER TABLE `other_staff` DISABLE KEYS */;
+/*!40000 ALTER TABLE `other_staff` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `patient`
+--
+
+DROP TABLE IF EXISTS `patient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patient` (
+  `SSN` int(9) unsigned NOT NULL,
+  `Notes` varchar(255) DEFAULT NULL,
+  `Date_admitted` varchar(20) DEFAULT NULL,
+  `Date_discharged` varchar(20) DEFAULT NULL,
+  `hospital_address` varchar(40) NOT NULL,
+  PRIMARY KEY (`SSN`,`hospital_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patient`
+--
+
+LOCK TABLES `patient` WRITE;
+/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal_details`
+--
+
+DROP TABLE IF EXISTS `personal_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personal_details` (
+  `patient_ssn` int(9) unsigned NOT NULL,
+  `age` int(3) unsigned DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `sex` varchar(4) DEFAULT NULL,
+  `birthdate` varchar(10) DEFAULT NULL,
+  `address` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal_details`
+--
+
+LOCK TABLES `personal_details` WRITE;
+/*!40000 ALTER TABLE `personal_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `personal_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `related_to`
+--
+
+DROP TABLE IF EXISTS `related_to`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `related_to` (
+  `patient_ssn` int(9) unsigned NOT NULL,
+  `contact_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`patient_ssn`,`contact_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `related_to`
+--
+
+LOCK TABLES `related_to` WRITE;
+/*!40000 ALTER TABLE `related_to` DISABLE KEYS */;
+/*!40000 ALTER TABLE `related_to` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
 
 DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `room` (
-  `Number` int NOT NULL,
-  `Hospital_Address` varchar(255) NOT NULL,
-  Foreign KEY (`Hospital_Address`) references hospital(`Address`),
-  PRIMARY KEY (`Number`,`Hospital_Address`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `number` int(11) NOT NULL,
+  `hospital_address` varchar(40) NOT NULL,
+  PRIMARY KEY (`number`,`hospital_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff`
+--
 
 DROP TABLE IF EXISTS `staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `staff` (
-  `StaffID` int NOT NULL,
-  `Access` varchar(255) DEFAULT NULL,
-  `Salary` varchar(255) DEFAULT NULL,
-  `Hospital_Address` varchar(255) NOT NULL,
-  Foreign KEY (`Hospital_Address`) references hospital(`Address`),
-  PRIMARY KEY (`staffID`,`Hospital_Address`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `staffid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `access` varchar(10) NOT NULL,
+  `salary` int(10) unsigned NOT NULL,
+  `hospital_address` varchar(40) NOT NULL,
+  PRIMARY KEY (`staffid`,`hospital_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `Patient`;
-CREATE TABLE `Patient` (
-  `SSN` int NOT NULL,
-  `Notes` varchar(255) DEFAULT NULL,
-  `Date_admitted` varchar(255) DEFAULT NULL,
-  `Date_discharged` varchar(255) DEFAULT NULL,
-  `Hospital_Address` varchar(255) NOT NULL,
-  Foreign KEY (`Hospital_Address`) references hospital(`Address`),
-  PRIMARY KEY (`SSN`,`Hospital_Address`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+--
+-- Dumping data for table `staff`
+--
 
-DROP TABLE IF EXISTS `Emergency_Contact`;
-CREATE TABLE `Emergency_Contact` (
-  `Name`  varchar(255) NOT NULL,
-  `Phone_number` int Default Null,
-  `Relationship` varchar(255) Default Null,
-  `Patient_SSN` int NOT NULL,
-  Foreign KEY (`Patient_SSN`) references Patient(`SSN`),
-  PRIMARY KEY (`Name`, `Patient_SSN`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+LOCK TABLES `staff` WRITE;
+/*!40000 ALTER TABLE `staff` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff` ENABLE KEYS */;
+UNLOCK TABLES;
 
-DROP TABLE IF EXISTS `Related_to`;
-CREATE TABLE `Related_to` (
-  `Patient_SSN`  int NOT NULL,
-  `Contact_Name` varchar(255) NOT NULL,
-  Foreign KEY (`Patient_SSN`) references Patient(`SSN`),
-  Foreign KEY (`Contact_Name`) references Emergency_Contact(`Name`),
-  PRIMARY KEY (`Patient_SSN`,`Contact_Name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+--
+-- Table structure for table `supplies`
+--
 
-DROP TABLE IF EXISTS `Personal_Details`;
-CREATE TABLE `Personal_Details` (
-  `Patient_SSN`  int NOT NULL,
-  `Age` int Default NULL,
-  `Name` varchar(255) Default NULL,
-  `Sex` varchar(16) Default NULL,
-  `Birthdate` varchar(255) Default NULL,
-  `Address` varchar(255) Default NULL,
-  Foreign KEY (`Patient_SSN`) references Patient(`SSN`)
-  PRIMARY KEY (`Patient_SSN`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `supplies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `supplies` (
+  `amount` int(11) unsigned NULL,
+  `equipment_name` varchar(40) NOT NULL,
+  `equipment_manufacturer` varchar(40) NOT NULL,
+  `equipment_type` varchar(40) NOT NULL,
+  PRIMARY KEY (`equipment_name`,`equipment_manufacturer`,`equipment_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `Appointment`;
-CREATE TABLE `Appointment` (
-  `Room_Number`  int NOT NULL,
-  `Staff_StaffID`  int NOT NULL,
-  `Patient_SSN`  int NOT NULL,
-  `Time`  varchar(255) Default NULL,
-  Foreign KEY (`Room_number`) references room(`number`),
-  Foreign KEY (`Staff_StaffID`) references staff(`StaffID`),
-  Foreign KEY (`Patient_SSN`) references Patient(`SSN`),
-  PRIMARY KEY (`Room_Number`,` Staff_StaffID`, `Patient_SSN`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+--
+-- Dumping data for table `supplies`
+--
 
-DROP TABLE IF EXISTS `Machine`;
-CREATE TABLE `Machine` (
-  `Equipment_Name`  varchar(255) NOT NULL,
-  `Equipment_Manufacturer`  varchar(255) NOT NULL,
-  `Equipment_Type`  varchar(255) NOT NULL,
-  `Room_Number`  int NOT NULL,
-  Foreign KEY (`Equipment_Name`) references equipment(`name`),
-  Foreign KEY (`Equipment_Manufacturer`) references equipment(`manufacturer`),
-  Foreign KEY (`Equipment_Type`) references equipment(`Type`),
-  Foreign KEY (`Room_Number`) references room(`number`),
-  PRIMARY KEY (`Equipment_Name`, `Equipment_Manufacturer`, `Equipment_Type`, `Room_Number`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+LOCK TABLES `supplies` WRITE;
+/*!40000 ALTER TABLE `supplies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `supplies` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-DROP TABLE IF EXISTS `Supplies`;
-CREATE TABLE `Supplies` (
-  `Amount`  int Default NULL,
-  `Equipment_Name`  varchar(255) NOT NULL,
-  `Equipment_Manufacturer`  varchar(255) NOT NULL,
-  `Equipment_Type`  varchar(255) NOT NULL,
-  Foreign KEY (`Equipment_Name`) references equipment(`name`),
-  Foreign KEY (`Equipment_Manufacturer`) references equipment(`manufacturer`),
-  Foreign KEY (`Equipment_Type`) references equipment(`Type`),
-  PRIMARY KEY (`Equipment_Name`, `Equipment_Manufacturer`, `Equipment_Type`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-DROP TABLE IF EXISTS `Other_Staff`;
-CREATE TABLE `Other_Staff` (
-  `Title`  varchar(255) Default NULL,
-  `Staff_StaffID` int NOT NULL,
-  `Staff_Access` varchar(255) Default NULL,
-  `Staff_Salary` int Default NULL,
-  Foreign KEY (`Staff_StaffID`) references staff(`StaffID`),
-  Foreign KEY (`Staff_Access`) references staff(`Access`),
-  Foreign KEY (`Staff_Salary`) references staff(`Salary`),
-  PRIMARY KEY (`Staff_StaffID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `Medical_Staff`;
-CREATE TABLE `Medical_Staff` (
-  `Specialization`  varchar(255) Default NULL,
-  `Staff_StaffID` int NOT NULL,
-  `Staff_Access` varchar(255) Default NULL,
-  `Staff_Salary` int Default NULL,
-  Foreign KEY (`Staff_StaffID`) references staff(`StaffID`),
-  Foreign KEY (`Staff_Access`) references staff(`Access`),
-  Foreign KEY (`Staff_Salary`) references staff(`Salary`),
-  PRIMARY KEY (`Staff_StaffID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+-- Dump completed on 2016-04-14 14:34:14
